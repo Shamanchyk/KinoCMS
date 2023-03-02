e0fbd09e102bd2f23ff424fa05cbd663f52bc5fd
Shamanchyk <vladhalibroda@gmail.com>
2023-03-02 21:48:17 +0800

    init

---
+++ js/433.55acfcb2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkinocms"]=self["webpackChunkkinocms"]||[]).push([[433],{433:function(n,r,e){e.r(r),e.d(r,{default:function(){return a}});var u=e(252);function c(n,r,e,c,t,i){return(0,u.wg)(),(0,u.iD)("div")}var t={},i=e(744);const s=(0,i.Z)(t,[["render",c]]);var a=s}}]);
+//# sourceMappingURL=433.55acfcb2.js.map
